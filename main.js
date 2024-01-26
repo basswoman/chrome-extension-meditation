503422a665506feb3a0f49cbca35ecf5f4c6bd21
Sincere Tuitt <sinceretuitt@gmail.com>
2024-01-25 21:00:44 -0500

    THe start!

---
+++ main.js
@@ -0,0 +1,6 @@
+// event listener for the button click that loads page --> visuals and music
+const medButton = document.getElementById('load-meditation-page');
+medButton.addEventListener('click', () => {
+    const title = document.createElement('h1')
+    title.createAttribute('id')
+})
