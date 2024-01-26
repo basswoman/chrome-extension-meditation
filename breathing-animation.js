a4b813f0fee6972eccd6a6dc505f2fde0dd9b3da
Sincere Tuitt <sinceretuitt@gmail.com>
2024-01-25 22:56:29 -0500

    few more improvements

--- breathing-animation.js
+++ breathing-animation.js
@@ -1,21 +1,21 @@
 class Animation {
   constructor (element) {
-    this.animation = document.createElement();
+    this.animation = document.createElement('div');
     this.animation.id = 'animation';
     element.appendChild(this.animation);
     
-    this.inhaleSpeed = 400 // increment between 
-    this.holdTime = 2000
-    this.exhaleSpeed = 800
+    this.inhaleSpeed = 400; // increment between 
+    this.holdTime = 2000;
+    this.exhaleSpeed = 800;
   }
 
   // a method for the growth animation
   grow () {
     function increase () {
         const currentHeight = Number(this.animation.height.replace('%', ''));
-        this.animation.setAttribute('height', `${currentHeight * 1.5}%`)
+        this.animation.setAttribute('height', `${currentHeight * 1.25892541179}%`)
         const currentLength = Number(this.animation.length.replace('%', ''));
-        this.animation.setAttribute('length', `${currentLength * 1.5}%`)
+        this.animation.setAttribute('length', `${currentLength * 1.25892541179}%`)
     }
   }
 
--- content.js
+++ content.js
@@ -1,14 +1,15 @@
 function start () {
-    console.log('are we in here?')
-            const animationContainer = document.createElement('div');
-            animationContainer.id = 'animationContainer';
-            document.querySelector('body').appendChild(animationContainer);
-    
-            const title = document.createElement('h1');
-            title.id = 'title';
-            title.innerText = 'Begin Meditation';
-            animationContainer.appendChild(title);
-        
-          const animationFunc = new Animation();
-    
-        }
+    const title = document.createElement('h1');
+    title.id = 'title';
+    title.innerText = 'Begin Meditation';
+    const body = document.querySelector('body');
+    body.appendChild(title);
+
+    const animationContainer = document.createElement('div');
+    animationContainer.id = 'animationContainer';
+    body.appendChild(animationContainer);
+
+
+    const animationFunc = new Animation(animationContainer);
+
+}
