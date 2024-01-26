26893f9381af1d5fc7b203b5ea93978d206b2fb9
Sincere Tuitt <sinceretuitt@gmail.com>
2024-01-25 22:22:51 -0500

    we have some functionality!

--- main.js
+++ main.js
@@ -4,10 +4,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 const medButton = document.getElementById('load-meditation-page');
 medButton.addEventListener('click', () => {
-   
-    // send a message to the content script (content.js) to change the webpage when the button is clicked.
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "load-page" });
-      });
+    start();
     })
 })
