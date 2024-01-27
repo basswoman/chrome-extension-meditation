7f40a35f8da75f6c052544e00e6a469142cdbbda
basswoman <54603761+basswoman@users.noreply.github.com>
2024-01-26 20:06:43 -0500

    add audio

--- content.js
+++ content.js
@@ -1,16 +1,17 @@
-function start () {
-    const title = document.createElement('h1');
-    title.id = 'title';
-    title.innerText = 'Begin Meditation';
-    const body = document.querySelector('body');
-    body.appendChild(title);
+function start() {
+  const title = document.createElement('h1');
+  title.id = 'title';
+  title.innerText = 'Begin Meditation';
+  const body = document.querySelector('body');
+  body.appendChild(title);
 
-    const animationContainer = document.createElement('div');
-    animationContainer.id = 'animationContainer';
-    body.appendChild(animationContainer);
+  const animationContainer = document.createElement('div');
+  animationContainer.id = 'animationContainer';
+  body.appendChild(animationContainer);
 
+  const animationFunc = new Animation(animationContainer);
+  console.log(document.getElementById('animation').attributes);
 
-    const animationFunc = new Animation(animationContainer);
-    console.log(document.getElementById('animation').attributes)
-
+  const audio = new Audio('audio.wav');
+  audio.play();
 }
