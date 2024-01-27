37ac5651f124183dce87bc5b5983450c02623316
basswoman <54603761+basswoman@users.noreply.github.com>
2024-01-26 20:34:31 -0500

    add hover

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // event listener for the button click that loads page --> visuals and music
 
-
-document.addEventListener("DOMContentLoaded", () => {
-const medButton = document.getElementById('load-meditation-page');
-medButton.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const medButton = document.getElementById('load-meditation-page');
+  medButton.addEventListener('click', () => {
     start();
-    })
-})
+    setTimeout(() => {
+      medButton.innerText = 'Press to finish';
+    }, 2000);
+  });
+});
